ecb0a26800bb3d0ff04dc5827db268bb08dc5139
bvalette <bvalette@student.42.fr>
2022-04-15 13:11:27 +0200

    seed/add - Links seed for each List

--- db/seeds/links.seed.js
+++ db/seeds/links.seed.js
@@ -7,27 +7,35 @@ const List = require('../../models/List.model')
 const Name = require('../../models/nameModels/Name.model')
 const Link = require('../../models/Link.model')
 
-// Create function to generate listId
-// argument for the function is number of lists(quantity)
-async function generateLinks(quantity) {
-  const links = []
+// generate some Links for a given ListId
+function generateLinksForOneList(quantity, listId, names) {
+  const links = [];
+  for (let j = 0; j < quantity; j++) {
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    const randomWeight = Math.floor(Math.random() * 11) - 1
+    let link = {
+      list: listId,
+      name: randomName._id,
+      weight: randomWeight,
+    }
+    console.log(`Seeding list ${listId} with ${randomName.value} of weight ${randomWeight}`);
+    links.push(link)
+  }
+  return links;
+}
+
+// Iterates on every existing Lists in the db and generate some Links for it.
+async function generateLinks() {
+  let links = []
   const lists = await List.find()
   const listIds = lists.map((list) => list._id)
-  const names = (await Name.find()).map((name) => name._id)
+  const names = await Name.find();
 
-  for (let i = 0; i < quantity; i++) {
-    const randomListId = listIds[Math.floor(Math.random() * listIds.length)]
-    const randomNumber = Math.floor(Math.random() * 5) + 5
-    for (let j = 0; j < randomNumber; j++) {
-      const randomNameId = names[Math.floor(Math.random() * names.length)]
-      const randomWeight = Math.floor(Math.random() * 11) - 1
-      let link = {
-        list: randomListId,
-        name: randomNameId,
-        weight: randomWeight,
-      }
-      links.push(link)
-    }
+  for (let i = 0; i < listIds.length; i++) {
+    const randomQuantity = Math.floor(Math.random() * 5) + 5
+    console.log(`✅ Seeding ${randomQuantity} links for the list ${listIds[i]}`);
+    const currentListLinks = generateLinksForOneList(randomQuantity, listIds[i], names)
+    links = [...links, ...currentListLinks];
   }
   return links
 }
@@ -39,7 +47,7 @@ async function seedAllLinks(linksToSeed) {
 const perform = async () => {
   try {
     await connect()
-    const linksToSeed = await generateLinks(3)
+    const linksToSeed = await generateLinks()
     await seedAllLinks(linksToSeed)
     await mongoose.connection.close()
   } catch (error) {
