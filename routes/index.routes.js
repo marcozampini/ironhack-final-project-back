4c7e8791567deae1eb5a1481a67519b0cb313ef6
bvalette <bvalette@student.42.fr>
2022-04-15 15:45:11 +0200

    routes/add - user search

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.routes");
+const usersRoutes = require("./users.routes");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -7,5 +8,6 @@ router.get("/", (req, res, next) => {
 });
 
 router.use("/auth", authRoutes);
+router.use("/users", usersRoutes);
 
 module.exports = router;
