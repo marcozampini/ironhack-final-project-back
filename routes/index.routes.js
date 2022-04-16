c5c1529f6400003b4faf23f54991716dfdfef363
Marco Zampini <marco@bloom-it.io>
2022-04-16 02:40:50 +0200

    routes/boards - Get all boards for the current user

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,13 @@
-const router = require("express").Router();
-const authRoutes = require("./auth.routes");
+const router = require('express').Router()
+const authRoutes = require('./auth.routes')
+const boardRoutes = require('./board.routes')
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
+router.get('/', (req, res, next) => {
+  res.json('All good in here')
+})
 
-router.use("/auth", authRoutes);
+router.use('/auth', authRoutes)
+router.use('/boards', boardRoutes)
 
-module.exports = router;
+module.exports = router
