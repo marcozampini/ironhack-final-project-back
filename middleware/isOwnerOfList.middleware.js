393e54fad835a7a0e0330845fc8fe2843a3db5cd
Akanksha Singh PRADHAN <95655583+Pradhan-90@users.noreply.github.com>
2022-04-18 10:39:20 +0200

    list route/ route created to add name and delete name from list

---
+++ middleware/isOwnerOfList.middleware.js
@@ -0,0 +1,22 @@
+const jwt_decode = require('jwt-decode')
+const List = require('../models/List.model')
+const status = require('http-status')
+
+async function isOwnerOfList(req, res, next) {
+  const list = await List.findById(req.params.listId)
+  if (!list) {
+    return res.status(status.NOT_FOUND).send(`List doesn't exist!!`)
+  }
+  const user = req.user
+  if (list.owner.toString() === user._id) {
+    next()
+  } else {
+    return res
+      .status(status.FORBIDDEN)
+      .send(`List doesn't belong to this user!!`)
+  }
+}
+
+module.exports = {
+  isOwnerOfList,
+}
