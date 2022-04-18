68219b82649f6a5beb7e7f9ba18460e10ce6acd7
bvalette <bvalette@student.42.fr>
2022-04-18 15:17:41 +0200

    nameRoute/add - route to fetch names based on search criterias

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,15 @@
-const router = require("express").Router();
-const authRoutes = require("./auth.routes");
-const usersRoutes = require("./user.routes");
+const router = require('express').Router()
+const authRoutes = require('./auth.routes')
+const userRoutes = require('./user.routes')
+const nameRoutes = require('./name.routes')
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.json("All good in here");
-});
+router.get('/', (req, res, next) => {
+  res.json('All good in here')
+})
 
-router.use("/auth", authRoutes);
-router.use("/users", usersRoutes);
+router.use('/auth', authRoutes)
+router.use('/users', userRoutes)
+router.use('/names', nameRoutes)
 
-module.exports = router;
+module.exports = router
