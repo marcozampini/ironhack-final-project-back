e828cae04072c016d531f2061a301c31bc643eb9
bvalette <bvalette@student.42.fr>
2022-04-21 19:13:40 +0200

    nameRoutes/fix - edge cases and route overlaps

--- routes/name.routes.js
+++ routes/name.routes.js
@@ -4,6 +4,7 @@ const searchNames = require('../controllers/searchName.controller')
 const httpStatus = require('http-status')
 const NameStats = require('../models/nameModels/NameStats.model')
 const Country = require('../models/Country.model')
+const ObjectID = require('mongodb').ObjectID
 
 /*
   Returns a list of names based on research criteria:
@@ -41,7 +42,7 @@ router.get('/:nameId', async (req, res, next) => {
     const results = await NameStats.find({ name: nameId }).populate(
       'name country'
     )
-    if (!results.length) {
+    if (!results || !results.length) {
       return res.sendStatus(httpStatus.NOT_FOUND)
     }
 
@@ -56,7 +57,6 @@ router.get('/:nameId', async (req, res, next) => {
       console.log('STATS COUNTRY', stat.country)
       output.countries.push(stat.country)
     })
-    console.log(results)
 
     output.totalCounts = results.reduce(
       (prev, current) => {
@@ -67,10 +67,11 @@ router.get('/:nameId', async (req, res, next) => {
       { mCount: 0, fCount: 0 }
     )
 
-    console.log(output)
     return res.status(httpStatus.OK).send(output)
   } catch (error) {
-    console.log('ERROR', error)
+    if (error.name === 'CastError') {
+      return res.sendStatus(httpStatus.NOT_FOUND)
+    }
     next(error)
   }
 })
@@ -79,11 +80,14 @@ router.get('/tops/:cca3', async (req, res, next) => {
   const cca3 = req.params?.cca3.toUpperCase()
   const limit = req.query?.limit || 100
 
-  if (!cca3) {
+  if (!cca3 || !cca3.length) {
     return res.sendStatus(httpStatus.BAD_REQUEST)
   }
   try {
     const country = await Country.findOne({ cca3: cca3 })
+    if (!country) {
+      return res.sendStatus(httpStatus.NOT_FOUND)
+    }
     const topBoysNames = await NameStats.find({ country: country._id })
       .sort({ mCount: -1 })
       .limit(limit)
@@ -98,7 +102,6 @@ router.get('/tops/:cca3', async (req, res, next) => {
       topBoysNames,
       topGirlsNames,
     }
-
     return res.status(httpStatus.OK).send(output)
   } catch (error) {
     console.log('ERROR', error)
