b0a6aa4e6552c7d95b8eec2f0143a0c525b32264
bvalette <bvalette@student.42.fr>
2022-04-21 18:57:10 +0200

    nameRoute/add - top names for given country with limit

--- routes/name.routes.js
+++ routes/name.routes.js
@@ -3,6 +3,7 @@ const { isAuthenticated } = require('../middleware/jwt.middleware')
 const searchNames = require('../controllers/searchName.controller')
 const httpStatus = require('http-status')
 const NameStats = require('../models/nameModels/NameStats.model')
+const Country = require('../models/Country.model')
 
 /*
   Returns a list of names based on research criteria:
@@ -31,8 +32,11 @@ router.get('/', isAuthenticated, async (req, res, next) => {
   return searchNames(req, res)
 })
 
-router.get('/:nameId', isAuthenticated, async (req, res, next) => {
+router.get('/:nameId', async (req, res, next) => {
   const { nameId } = req.params
+  if (!nameId) {
+    return res.sendStatus(httpStatus.BAD_REQUEST)
+  }
   try {
     const results = await NameStats.find({ name: nameId }).populate(
       'name country'
@@ -47,20 +51,54 @@ router.get('/:nameId', isAuthenticated, async (req, res, next) => {
       countries: [],
     }
     results.forEach((stat) => {
-      stat.country._doc.mCount = stat.mCount;
-      stat.country._doc.fCount = stat.fCount;
-      console.log('STATS COUNTRY', stat.country);
+      stat.country._doc.mCount = stat.mCount
+      stat.country._doc.fCount = stat.fCount
+      console.log('STATS COUNTRY', stat.country)
       output.countries.push(stat.country)
     })
-    console.log(results);
+    console.log(results)
 
-    output.totalCounts = results.reduce((prev, current) => {
-      prev.mCount += current?.mCount || 0;
-      prev.fCount += current?.fCount || 0;
-      return prev;
-    }, { mCount: 0, fCount: 0})
+    output.totalCounts = results.reduce(
+      (prev, current) => {
+        prev.mCount += current?.mCount || 0
+        prev.fCount += current?.fCount || 0
+        return prev
+      },
+      { mCount: 0, fCount: 0 }
+    )
 
     console.log(output)
+    return res.status(httpStatus.OK).send(output)
+  } catch (error) {
+    console.log('ERROR', error)
+    next(error)
+  }
+})
+
+router.get('/tops/:cca3', async (req, res, next) => {
+  const cca3 = req.params?.cca3.toUpperCase()
+  const limit = req.query?.limit || 100
+
+  if (!cca3) {
+    return res.sendStatus(httpStatus.BAD_REQUEST)
+  }
+  try {
+    const country = await Country.findOne({ cca3: cca3 })
+    const topBoysNames = await NameStats.find({ country: country._id })
+      .sort({ mCount: -1 })
+      .limit(limit)
+      .populate('name')
+    const topGirlsNames = await NameStats.find({ country: country._id })
+      .sort({ fCount: -1 })
+      .limit(limit)
+      .populate('name')
+
+    const output = {
+      country,
+      topBoysNames,
+      topGirlsNames,
+    }
+
     return res.status(httpStatus.OK).send(output)
   } catch (error) {
     console.log('ERROR', error)
