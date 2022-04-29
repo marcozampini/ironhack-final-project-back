c7cb6a9579a18a6692d3bb8a767029b41d1fb62f
Marco Zampini <marco@bloom-it.io>
2022-04-29 05:46:09 +0200

    model/nameStats Count per gender, add rank

--- models/nameModels/NameStats.model.js
+++ models/nameModels/NameStats.model.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose')
 const Name = require('./Name.model')
 const Country = require('../Country.model')
 /*
@@ -10,15 +10,19 @@ const nameStatsSchema = new Schema(
   {
     name: { type: Schema.Types.ObjectId, ref: 'Name', required: true },
     country: { type: Schema.Types.ObjectId, ref: 'Country', required: true },
-    mCount: { type: Number, default: 0 },
-    fCount: { type: Number, default: 0 },
+    gender: {
+      type: String,
+      required: true,
+    },
+    count: { type: Number, default: 0 },
+    rank: { type: Number, default: 0 },
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
   }
-);
+)
 
-const NameStats = model("NameStats", nameStatsSchema);
+const NameStats = model('NameStats', nameStatsSchema)
 
-module.exports = NameStats;
+module.exports = NameStats
